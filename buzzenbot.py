322e95d9c63809ad4e60211a7d03f11ee9f1ea71
JanEhehalt <jan.ehehalt@gmail.com>
2024-05-26 20:10:37 +0200

    Role management löbbt

--- buzzenbot.py
+++ buzzenbot.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 import discord
+from discord import *
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 # -------------------------------------
-prefix = "!"
 path_to_token = "./.token"
 # -------------------------------------
 
@@ -17,27 +17,85 @@ with open(path_to_token, "r", encoding="utf8") as f:
     token = f.read().splitlines()[0]
 
 
+# The bot will send its message for reactions into this channel id
+reaction_message_channel_id: str = "1244337390667956295"
+reaction_message_channel: TextChannel = None
+
+# Emoji -> role which is given when reacting with the emoji
+emoji_role_map: dict = {
+    "🇬🇧": "EN",
+    "🇩🇪": "DE",
+    "⚔️": "Clan War Participant",
+    "🛡️": "Clan War Reservist"
+}
+
+# building the message which will be sent to be reacted to 
+reaction_message_content: str = "React to this message to receive the roles you want:"
+
+for emoji in emoji_role_map.keys():
+    reaction_message_content += "\n"
+    reaction_message_content += emoji + " : " + emoji_role_map[emoji]
+
+
 @client.event
 async def on_ready():
-    print(f"Logged in as {client.user}")
+    reaction_message_channel = await client.fetch_channel(reaction_message_channel_id)
+    await reaction_message_channel.send(reaction_message_content)
+
+    print(f"Logged in as {client.user}.")
 
 
 @client.event
-async def on_message(message):
-    if message.author == client.user:
+async def on_message(message: Message):
+    if message.author != client.user: # not adding reactions to foreign messages
+        return
+    
+    # adding reaction to the message which was sent in on_ready
+    for emoji in emoji_role_map.keys():
+        await message.add_reaction(emoji)
+
+@client.event
+async def on_reaction_add(reaction: Reaction, user: User) -> None:
+    if reaction.message.author != client.user: # not reacting to foreign messages
+        return
+    if user == client.user: # not reacting to the bots own reaction
+        return
+    
+    # checking if reaction emoji is configured in our dict
+    role_name = emoji_role_map[str(reaction.emoji)]
+    if not role_name:
+       return
+    
+    # checking if configured role from dict exists on the server
+    guild = reaction.message.guild
+    role = discord.utils.get(guild.roles, name=role_name)
+    if not role:
         return
 
-    if not message.channel.name == "bot" or message.channel.name == "bot-befehle" or message.channel.id == 1091452282878562405:
+    # granting role
+    await user.add_roles(role)
+    print("Gave role " + role.name + " to " + user.name)
+
+@client.event
+async def on_reaction_remove(reaction: Reaction, user: User) -> None:
+    if reaction.message.author != client.user: # not reacting to foreign messages
+        return
+    if user == client.user: # not reacting to the bots own reaction
         return
 
-    args = message.content.split(" ")
-    response = "Kein bekannter Befehl. Versuch mal " + prefix + "help, um Hilfe zu bekommen"
+    # checking if reaction emoji is configured in our dict
+    role_name = emoji_role_map[str(reaction.emoji)]
+    if not role_name:
+       return
 
-    if not args[0].startswith(prefix):
+    # checking if configured role from dict exists on the server
+    guild = reaction.message.guild
+    role = discord.utils.get(guild.roles, name=role_name)
+    if not role:
         return
-    if args[0] == (prefix + 'ping'):
-        response = "pong"
 
-    await message.channel.send(response, reference=message)
+    # removing role
+    await user.remove_roles(role)
+    print("Removed role " + role.name + " from " + user.name)
 
 client.run(token)
